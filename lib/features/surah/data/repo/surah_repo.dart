bb5b105daf5cc3e5c0bcc949b5f406badc809678
Fouad Salama <fs2030565@gmail.com>
2023-12-17 06:53:51 +0200

    add model again

--- lib/features/surah/data/repo/surah_repo.dart
+++ lib/features/surah/data/repo/surah_repo.dart
@@ -1,1 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:islamic_app/core/errors/failure.dart';
 
+import '../model/surah_details_model/surah_details_model.dart';
+
+abstract class SurahRepo {
+  Future<Either<Failure, List<SurahDetailsModel>>> fetchSurahDetails();
+}
